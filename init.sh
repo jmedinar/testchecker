889093e5bb7eb7337bf270c3926831f2dd1e5845
Juan Medina <jumedina@redhat.com>
2024-04-04 12:48:45 -0500

    init

---
+++ init.sh
@@ -0,0 +1,79 @@
+#!/urs/bin/env bash
+# Script: init.sh
+# Author: Professor Juan Medina
+# Email: jmedina@collin.edu
+# Date: Apr 2024
+version="1.0"
+# Purpose: This script will setup the environment requirements for the 
+#          class ITSC-1316.253: Linux Install & Configuration.
+
+# Red             Green          Yellow            Blue
+CR='\e[0;31m' CG='\e[0;32m' CY='\e[0;33m' CL='\e[0;34m'
+# Purple          Cyan           White
+CP='\e[0;35m' CC='\e[0;36m' CW='\e[0;37m'
+
+# Verifying the System is not Running Under the Linux Installer
+if [[ ${USER} == "liveuser" ]]; then
+    echo -e "${CR}
+    Notice that your shell indicates your current user is 'liveuser' 
+    which suggests that you are still running under the ${CY} LIVE ISO Linux Installer ${CR}
+    and still need to complete the Linux Installation process!
+    ${CY}
+    Please remove the ISO from the Virtual Machine and reboot it to remediate this.
+    ${CL}
+    This action will enable you to complete the Linux Installation process.
+    Once completed, you will be able to execute the init.sh, script successfully! ${CW}"
+    exit 1
+fi
+
+# Verifying the Script was Executed with root Privileges
+if [[ ${UID} -ne 0 ]]; then
+    echo -e "${CR}
+    This script must be executed with sudo to acquire administrator (root) privileges!
+    Example: ${CY} sudo bash ~/Downloads/init.sh ${CW}"
+    exit 2
+fi
+
+# Verifying Internet Connection
+if [[ $( wget -q --spider http://google.com; echo $?) -ne 0 ]]; then
+    echo -e "${CR} Your Virtual Machine is currently not connected to the internet!
+    ${CY}
+    Connect it by clicking on the icons at the top right corner of Linux 
+    and ensuring the Wi-Fi is enabled. 
+    ${CL}
+    Once corrected, rerun this script. ${CW}"
+    exit 3
+fi
+
+echo -e "${CY} Setting up the testchecker tool..."
+wget -q /home/$student/Downloads/testchecker.sh -P /usr/bin/testchecker
+chmod +x /usr/bin/testchecker
+
+echo -e "${CY} Setting up the prompt..."
+echo "
+if [[ $EUID -eq 0 ]]; then C=31; else C=32; fi
+SC="\[\e[${C}m\]"
+RC="\[\e[0m\]"
+export PS1="${SC}\u@\h \w${RC}\$ "
+" >> /etc/bashrc
+
+echo -e "${CY} Setting up the hostname..."
+hostnamectl set-hostname fedora
+
+echo -e "${CG} Your environment has been set up successfully! ${CW}
+
+1. You now have the ${CY} testchecker tool ${CW} installed in your system.
+
+The testchecker tool helps you verify your assignments. 
+
+   To run it, execute the following command: 
+
+       ${CY} sudo testchecker ${CW}
+
+2. The hostname has been set as fedora.
+
+3. You will notice that the prompt in all future terminals will be colored,
+${CG} green ${CW} when working as a regular user account and in ${CR} red ${CW} when 
+running as the administrator (root) account. 
+
+${CY} To continue, please close this terminal window and open a new one.${CW}"
---
+++ testchecker.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+# Script: testChecker.sh
+title="TEST CHECKER"
+# Author: Professor Juan Medina
+# Email: jmedina@collin.edu
+# Date: 03/23/2024
+version="2.1"
+# Purpose: This script will gatter VM and Student information
+#          and will execute the interactive test verification scripts
+
+# Red             Green          Yellow            Blue
+CR='\e[0;31m' CG='\e[0;32m' CY='\e[0;33m' CL='\e[0;34m'
+# Purple          Cyan           White
+CP='\e[0;35m' CC='\e[0;36m' CW='\e[0;37m'
+
+_run_as_root() {
+	# Verifying the Script was Executed with root Privileges
+	if [[ ${UID} -ne 0 ]]; then
+		echo -e "${CR}
+		This script must be executed with sudo to acquire administrator (root) privileges!
+		Example: ${CY} sudo bash ~/Downloads/init.sh ${CW}"
+		exit 2
+	fi
+}
+
+_internet_connection() {
+	# Verifying Internet Connection
+	if [[ $( wget -q --spider http://google.com; echo $?) -ne 0 ]]; then
+		echo -e "${CR} Your Virtual Machine is currently not connected to the internet!
+		${CY}
+		Connect it by clicking on the icons at the top right corner of Linux 
+		and ensuring the Wi-Fi is enabled. 
+		${CL}
+		Once corrected, rerun this script. ${CW}"
+		exit 3
+	fi
+}
+
+student=$(who am i | awk '{print $1}')
+uuid=$(dmidecode -s system-uuid)
+hostname=$(hostname -f)
+today=$(date)
+
+_run_as_root
+_internet_connection
+#TODO: Verify running on the last version
+
+clear
+echo ""
+echo -e "${CC} ========================================================================="
+echo -e "${CY}                        C O L L I N   C O L L E G E "
+echo -e "${CY}                         ${title} Version: ${version} "
+echo -e "${CC} ========================================================================="
+echo -e "${CG} DATE: ${CY} ${today} ${CG} STUDENT: ${CY} ${student} ${CW}"
+read -p " Introduce your student numeric ID: " studentid
+read -p " Indicate the assignment to check [from 1 to 8]: " choice
+case ${choice} in
+	1) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDEuc2gK" | base64 -d)) ;;
+	2) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDIuc2gK" | base64 -d)) ;;
+	3) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDMuc2gK" | base64 -d)) ;;
+	4) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDQuc2gK" | base64 -d)) ;;
+	5) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDUuc2gK" | base64 -d)) ;;
+	6) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDYuc2gK" | base64 -d)) ;;
+	7) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDcuc2gK" | base64 -d)) ;;
+	8) source <(curl -s $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDguc2gK" | base64 -d)) ;;
+	*) echo -e "${CR} Invalid choice. Exiting... ${CW} " && exit ;;
+esac
+echo -e "${CC} ========================================================================="
+echo -e "${CY} $(echo "${uuid},${student},${studentid},${hostname},${today}" | base64 -w 0)${CW}"
+echo ""
