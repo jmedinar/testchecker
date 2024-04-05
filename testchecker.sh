df5b8cd503ac7b8761b8fedba5f602d8ac6a5285
Juan Medina <jmedinar@gmail.com>
2024-04-05 08:53:30 -0500

    clean

--- testchecker.sh
+++ testchecker.sh
@@ -45,8 +45,6 @@ _update(){
 	# Check if running the latest version
 	code="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vdGVzdGNoZWNrZXIuc2gK" 
 	remote_version=$(curl -sk $(echo ${code} | base64 -d) | grep -E '^version' | sed 's/=/ /' | awk '{print $NF}' | sed 's/"//g')
-	echo "remote_version=$remote_version"
-	echo "current_version=$version"
 	if [[ $remote_version != $version ]]
 	then
 		echo -e "A new version of the testchecker is available. Upgrading..."
