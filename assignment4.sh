b55d35ae8860c2ba47e46740e144acd823e8f526
Juan Medina <jmedinar@gmail.com>
2024-04-06 06:35:43 -0500

    fix 4

--- assignment4.sh
+++ assignment4.sh
@@ -8,96 +8,51 @@
 CR='\e[0;31m' CG='\e[0;32m' CY='\e[0;33m' CL='\e[0;34m' CP='\e[0;35m' CC='\e[0;36m' CW='\e[0;37m'
 assignment=4
 ca=0
-tq=36  # Total number of user times verified items (9*4=36)
-
-echo -e "${CC} ========================================================================="
-echo -e "${CP} Assignment ${assignment} Verification"
-echo -e "${CC} ========================================================================="
+tq=36 # Total number of user times verified items (9*4=36)
 
 _user() {
-   exist=""
-   primary=""
-   auxiliar=""
-   gecos=""
-   if id ${1} &>/dev/null
-   then
-      exist="true"
-      ((ca++))
+	exist="false" 	primary="false" 	auxiliar="false" 	gecos="false"
 
-      # Primary group verification
-      if [[ "$(id -gn ${1})" == "${2}" ]]
-      then 
-         ((ca++))
-         primary="true"
-      else 
-         primary="false"
-      fi
+   # Check if the user exists
+	if id ${1} &>/dev/null; then exist="true"; ((ca++)); fi
 
-      # auxiliar groups verification
-      case ${2} in
-         accounting | technology) 
-            if [[ "$(userdbctl user ${1} | grep Aux)" == "" ]]
-            then 
-               ((ca++))
-               auxiliar="true"
-            else 
-               auxiliar="false"
-            fi 
-            ;;
-         humanresources)
-            for g in $(id -nG ${1})
-            do
-               if [[ ${g} != "humanresources" ]]
-               then
-                  if [[ ${g} == "accounting" ]]
-                  then 
-                     ((ca++)) 
-                     auxiliar="true"
-                  else 
-                     auxiliar="false"
-                  fi
-               fi
-            done
-            ;;
-         directionboard)  
-            count=0
-            for g in $(id -nG ${1})
-            do
-               if [[ ${g} != "directionboard" ]]
-               then
-                  if [[ ${g} == "technology" ]]; then ((count++))
-                  elif [[ ${g} == "humanresources" ]]; then ((count++))
-                  elif  [[ ${g} == "accounting" ]]; then ((count++)); fi
-               fi
-            done
-            if [[ ${count} -eq 3 ]]
-            then
-               ((ca++))
-               auxiliar="true"
-            else
-               auxiliar="false"
+   # Primary group verification
+   if [[ "$(id -gn ${1})" == "${2}" ]]; then ((ca++)); primary="true"; fi
+
+   # gecos field verification
+   if grep "${1}@wedbit.com" /etc/passwd &>/dev/null; then ((ca++)); gecos="true"; fi
+
+   # auxiliar groups verification
+   case ${2} in
+      "accounting" | "technology") # Only requires the primary group, so Aux must be empty
+         if [[ "$(userdbctl user ${1} | grep Aux)" == "" ]]; then ((ca++)); auxiliar="true"; fi ;;
+      "humanresources") # Requires accounting
+         for g in $(id -nG ${1}); do
+            if [[ ${g} != "humanresources" ]]; then
+               if [[ ${g} == "accounting" ]]; then ((ca++)); auxiliar="true"; fi
             fi
-            ;;
-         *) echo "Unknown"
-            ;;
-      esac
+         done ;;
+      "directionboard") # Requires the three other groups
+         count=0
+         for g in $(id -nG ${1}); do
+            if [[ ${g} != "directionboard" ]]; then
+               if [[ ${g} == "technology" ]]; then ((count++))
+               elif [[ ${g} == "humanresources" ]]; then ((count++))
+               elif [[ ${g} == "accounting" ]]; then ((count++)); fi
+            fi
+         done
+         if [[ ${count} -eq 3 ]]; then ((ca++)); auxiliar="true"; fi ;;
+      *) echo "Unknown" ;;
+   esac
 
-      # gecos field verification
-      if grep "${1}@wedbit.com" /etc/passwd &>/dev/null
-      then
-         ((ca++))
-         gecos="true"
-      else
-         gecos="false"
-      fi
-   else
-      exist="false"
-   fi
-   printf "${CY}%-20s%-20s%-20s%-20s%-20s${CW}\n" ${1} ${exist} ${primary} ${auxiliar} ${gecos}
+	printf "${CY}%-10s%-8s%-15s%-18s%-10s${CW}\n" ${1} ${exist} ${primary} ${auxiliar} ${gecos}
 }
 
-printf "${CG}%-20s%-20s%-20s%-20s%-20s${CW}\n"  USER EXIST PRIMARY AUXILIARY GECOS
-echo -e "${CL}=========================================================================================${CW}"
+echo -e "${CC}===================================================================="
+echo -e "${CP} Assignment ${assignment} Verification"
+echo -e "${CC}===================================================================="
+printf "${CG}%-10s%-8s%-15s%-18s%-10s${CW}\n" USER EXIST PRIMARY_GROUP AUXILIARY_GROUP GECOS_COMMENT
+echo -e "${CL}====================================================================${CW}"
 _user cyen accounting
 _user mpearl accounting
 _user jgreen directionboard
@@ -107,7 +62,7 @@ _user poto technology
 _user mkhan technology
 _user llopez directionboard
 _user jramirez humanresources
-echo -e "${CL}=========================================================================================${CW}"
+echo -e "${CL}====================================================================${CW}"
 
 printf "${CP} FINAL GRADE: ${CC} %.0f ${CW}" "$(echo "( 100 / ${tq} ) * ${ca}" | bc -l)"
 echo ""
