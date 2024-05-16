af8dba3dc49feeae08ead65908e9578b1f9d48e7
Juan Medina <jmedinar@gmail.com>
2024-05-16 10:27:01 -0500

    updates

--- assignment5.sh
+++ assignment5.sh
@@ -1,82 +1,97 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Script: assignment5.sh
 # Author: Professor Juan Medina
 # Email: jmedina@collin.edu
 # Date: 03/23/2024
 
-# Red           Green         Yellow        Blue          Purple        Cyan          White
+# Red         Green         Yellow        Blue          Purple        Cyan          White
 CR='\e[0;31m' CG='\e[0;32m' CY='\e[0;33m' CL='\e[0;34m' CP='\e[0;35m' CC='\e[0;36m' CW='\e[0;37m'
 assignment=5
-ca=0
-tq=4
-
-_msg() {
-   echo -ne "${CY} ${1}"
-}
-
-_pass() {
-   echo -e "${CG} true ${CR}"
-   ((ca++))
-}
-
-_fail() {
-   echo -e "${CR} false ${CG}"
-}
-
-_eval() {
-    if eval ${1} &>/dev/null; then _pass; else _fail; fi
-}
+grade=0
+bdir="/sysadm/bin"
+targets=(challenge1 challenge2 challenge3 challenge4 challenge5 challenge6 challenge7 challenge8 challenge9 \
+homeChecker rabbitJumps testString processFile userValidator)
 
 _print_line() {
     printf "${CC}%0.s=" {1..80}
     printf "${CW}\n"
 }
 
+_eval(){
+    target=$1     arg=$2     ret=$3     code=$4     r=false     points=0      value=7
+    # Evaluating output
+    if ${bdir}/${target}.sh ${arg} 2>/dev/null | grep -q "${ret}"
+    then
+        # Evaluating source code
+        #if grep -q "${code}" ${bdir}/${target}
+        #then
+            r=true
+            # grading
+            case ${target} in
+                            challenge*) (( points = value * 1 )); ((grade+=points)) ;;
+                processFile|testString) (( points = value * 3 )); ((grade+=points)) ;;
+                                     *) (( points = value * 2 )); ((grade+=points)) ;;
+            esac
+        #fi
+    fi
+    printf "${CG}%-30s${CY}%-20s%-20s${CW}\n" ${bdir}/${target}.sh ${r} ${points}
+}
+
 _print_line
 echo -e "${CP} Assignment ${assignment} Verification"
 _print_line
-
-base_dir="/sysadm/bin"
-targets=(processFile.sh rabbitJumps.sh testString.sh color.sh)
-
+printf "${CG}%-30s%-20s%-20s${CW}\n" SCRIPT RESULT POINTS
+_print_line
 for t in "${targets[@]}"
 do
-    if [[ -f ${base_dir}/${t} ]]
-    then
-        if [[ -x ${base_dir}/${t} ]]
-        then
-            _msg "Verifying ${t}"
-            case ${t} in
-                processFile.sh)
-                    if ${base_dir}/${t} /etc/passwd 2>/dev/null | grep "Method" &>/dev/null; then _pass; else _fail; fi
-                    ;;
-                rabbitJumps.sh)
-                    if ${base_dir}/${t} 2>/dev/null | grep "SUCCESS!" &>/dev/null; then _pass; else _fail; fi
-                    ;;
-                testString.sh)
-                    if ${base_dir}/${t} 1 2>/dev/null | grep "Binary or positive integer" &>/dev/null; then _pass; else _fail; fi
-                    ;;
-                color.sh)
-                    if ${base_dir}/${t} 2>/dev/null | grep -i LOVE &>/dev/null
-                    then
-                        if grep "echo -e" ${base_dir}/${t} &>/dev/null \
-                            || grep "printf" ${base_dir}/${t} &>/dev/null; then _pass; else _fail; fi
-                    else
-                        _fail
-                    fi
-                    ;;
-                *) echo "Unknown"
-                    ;;
-            esac
-        else
-            echo -e "${CR} ${t} cannot be executed! ${CW}"
-        fi
-    else
-        echo -e "${CR} ${base_dir}/${t} NOT FOUND! ${CW}"
-    fi
-done
+    case ${t} in
+        challenge1)  _eval      "${t}" "" \
+                                "Hello, world!" ;;
 
-printf "${CP} FINAL GRADE: ${CC} %.0f ${CW}" $(echo "(100/${tq})*${ca}" | bc -l)
+        challenge2)  _eval      "${t}" "" \
+                                "Linux" ;;
+
+        challenge3)  _eval      "${t}" "" \
+                                "Hello Alice, Welcome to Wonderland!" ;;
+
+        challenge4)  _eval      "${t}" "" \
+                                "File /etc/linux_version not found" ;;
+
+        challenge5)  _eval      "${t}" "Learning Linux challenging it also fun very rewarding" \
+                                "Learning Linux is challenging but it is also fun and very rewarding" ;;
+
+        challenge6)  _eval      "${t}" "" \
+                                "Multiplication: 50" ;;
+
+        challenge7)  _eval      "${t}" "" \
+                                "We are in the era of AI and agriculture is still more important in the year 2024!" ;;
+
+        challenge8)  _eval      "${t}" "" \
+                                "/etc/passwd exists!" ;;
+
+        challenge9)  _eval      "${t}" "" \
+                                ". Like in the Beatles song, Hello, Goodbye!" ;;
+
+        homeChecker) _eval      "${t}" "" \
+                                "/home/goneuser" ;;
+
+        processFile) _eval      "${t}" "/etc/passwd" \
+                                "Method" ;;
+
+        rabbitJumps) _eval      "${t}" "" \
+                                "The Rabbit made it!" ;;
+
+        testString)  _eval      "${t}" "1" \
+                                "Binary or positive integer" ;;
+
+        userValidator) _eval    "${t}" "" \
+                                "root is UNLOCKED" ;;
+
+        *) echo "Unknown" ;;
+    esac
+done
+_print_line
+printf "${CP} FINAL GRADE:${CC} %.0f${CW}" $grade
 echo ""
 
 # CHALLENGE:
