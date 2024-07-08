42a665766863bec5c237adfef32e62fef7d55c76
Juan Medina <jmedinar@gmail.com>
2024-07-08 16:48:30 -0500

    Update mod6-init.sh

--- performance/mod6-init.sh
+++ performance/mod6-init.sh
@@ -5,5 +5,5 @@
 
 if [[ ${UID} -ne 0 ]]; then echo "Execute this script with sudo"; exit 1; fi
 if [[ $(wget -q --spider http://google.com; echo $?) -ne 0 ]]; then echo "Internet connection required"; exit 2; fi
-source <(curl -sk -H 'Cache-Control: no-cache' $(echo "" | base64 -d))
+source <(curl -sk -H 'Cache-Control: no-cache' $(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vcGVyZm9ybWFuY2UvcGVyZi1jaGFsbGVuZ2UxLnNoCg==" | base64 -d))
 echo Done!; exit 0
