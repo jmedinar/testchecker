7d95909f0e75e4e36f3924869a11207b2222c3db
R0tt3nT0m4t0 <jmedinar@gmail.com>
2025-03-17 20:12:10 -0500

    finals and init

--- testchecker.sh
+++ testchecker.sh
@@ -4,46 +4,33 @@ title="TestChecker"
 # Author: Professor Juan Medina
 # Email: jmedina@collin.edu
 # Date: 03/23/2024
-version="3.0.1"
+version="4.0.1"
 # Purpose: This script will gater VM and Student information
 #          and will execute the interactive test verification scripts
 
 # Red         Green         Yellow        Blue  		Purple        Cyan          White
 CR='\e[0;31m' CG='\e[0;32m' CY='\e[0;33m' CL='\e[0;34m' CP='\e[0;35m' CC='\e[0;36m' CW='\e[0;37m'
-sname=$(who am i | awk '{print $1}')
+username=$(who am i | awk '{print $1}')
+version=$1
 sid=0
 
-_run_as_root() {
-	# Verifying the Script was Executed with root Privileges
-	if [[ ${UID} -ne 0 ]]; then
-		echo -e "${CR}
-		This script must be executed with sudo to acquire administrator (root) privileges!
-		Example: ${CY} sudo testchecker ${CW}"
-		exit 2
-	fi
-}
-
-_no_vboxuser() {
-	# Verify the student is not running as vboxuser
-	if [[ ${USER} == "vboxuser" ]]; then
-        echo -e "${CR}
-        You are currently running as the 'vboxuser' user, which is not allowed!. ${CW}"
-		exit 2
-	fi
-}
-
-_internet_connection() {
-	# Verifying Internet Connection
-	if [[ $( wget -q --spider http://google.com; echo $?) -ne 0 ]]; then
-		echo -e "${CR} Your Virtual Machine is currently not connected to the internet!
-		${CY}
-		Connect it by clicking on the icons at the top right corner of Linux 
-		and ensuring the Wi-Fi is enabled. 
-		${CL}
-		Once corrected, rerun this script. ${CW}"
-		exit 3
-	fi
-}
+if [[ ${UID} -ne 0 ]]; then echo "Execute this script with sudo"; exit 1; fi
+if id "liveuser" &>/dev/null || [ -f /etc/live-release ] || [[ "$(findmnt -n -o FSTYPE /)" =~ (squashfs|overlay) ]]; then
+    echo "Error: This script should not be run as the 'liveuser' or from the live ISO environment."
+    echo "Please complete the Fedora installation and log in as a regular user before running this script."
+    exit 1
+fi
+if id "vboxuser" &>/dev/null; then
+    echo "Error: This script should not be run as the 'vboxuser'."
+    echo "If using a computer at school ensure you are running under your own user account before running this script."
+    exit 1
+fi
+if ! grep -q '^ID=fedora$' /etc/os-release || [ ! -f /etc/fedora-release ] || ! command -v dnf &>/dev/null; then
+    echo "Error: This script is designed to run on Fedora Linux only."
+    echo "Please run this script on a Fedora Linux System."
+    exit 1
+fi
+if [[ $(wget -q --spider http://google.com; echo $?) -ne 0 ]]; then echo "Internet connection required"; exit 2; fi
 
 _update(){
 	# Check if running the latest version
@@ -71,9 +58,6 @@ _print_line() {
     printf "${CW}\n"
 }
 
-_run_as_root
-_no_vboxuser
-_internet_connection
 _update
 
 clear
@@ -82,26 +66,32 @@ _print_line
 echo -e "${CY}                        C O L L I N   C O L L E G E "
 echo -e "${CY}                        ${title} Version: ${version} "
 _print_line
-echo -e "${CG} DATE: ${CY} $(date) ${CG} STUDENT: ${CY} ${sname} ${CW}"
+echo -e "${CG} DATE: ${CY} $(date) ${CG} STUDENT: ${CY} ${username} ${CW}"
 read -p " Introduce your student numeric ID: " sid
-read -p " Indicate the assignment to check [from 1 to 8]: " choice
-case ${choice} in
-	1) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDEuc2gK" | base64 -d)) ;;
-	2) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDIuc2gK" | base64 -d)) ;;
-	3) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDMuc2gK" | base64 -d)) ;;
-	4) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDQuc2gK" | base64 -d)) ;;
-	5) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDUuc2gK" | base64 -d)) ;;
-	6) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDYuc2gK" | base64 -d)) ;;
-	7) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDcuc2gK" | base64 -d)) ;;
-	8) source <(curl -sk -H 'Cache-Control: no-cache' \
-				$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDguc2gK" | base64 -d)) ;;
-	*) echo -e "${CR} Invalid choice. Exiting... ${CW} " && exit ;;
-esac
+if [[ -z $version ]]
+then
+	read -p " Indicate the assignment to check [from 1 to 8]: " choice
+	case ${choice} in
+		1) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDEuc2gK" | base64 -d)) ;;
+		2) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDIuc2gK" | base64 -d)) ;;
+		3) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDMuc2gK" | base64 -d)) ;;
+		4) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDQuc2gK" | base64 -d)) ;;
+		5) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDUuc2gK" | base64 -d)) ;;
+		6) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDYuc2gK" | base64 -d)) ;;
+		7) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDcuc2gK" | base64 -d)) ;;
+		8) source <(curl -sk -H 'Cache-Control: no-cache' \
+					$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDguc2gK" | base64 -d)) ;;
+		*) echo -e "${CR} Invalid choice. Exiting... ${CW} " && exit ;;
+	esac
+else
+	source <(curl -sk -H 'Cache-Control: no-cache' \
+			$(echo "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2ptZWRpbmFyL3Rlc3RjaGVja2VyL21haW4vYXNzaWdubWVudDguc2gK" | base64 -d)) param1 param2
+fi
 _print_line
