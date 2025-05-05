af295f3041e314c39d73f74cd15c877049f27bb1
R0tt3nT0m4t0 <jmedinar@gmail.com>
2025-05-05 08:33:11 -0500

    fixed grade

--- assignment5.sh
+++ assignment5.sh
@@ -42,8 +42,6 @@ _check_script_output(){
     local script_basename="$1"
     local script_args="$2"
     local expected_output="$3"
-    # $4=code_pattern (currently unused/commented out)
-    # local expected_code_pattern="$4"
 
     local script_path="${script_base_dir}/${script_basename}.sh"
     local points_value=0
@@ -77,19 +75,9 @@ _check_script_output(){
         # Check if the output contains the expected substring
         # Using grep -F for fixed string matching is safer if no regex is needed
         elif echo "${script_output}" | grep -qF "${expected_output}"; then
-            # Check 3: Source Code Pattern (Optional - currently disabled)
-            # local code_check_passed=true # Assume true if check is disabled
-            # if [[ -n "${expected_code_pattern}" ]]; then
-            #     if ! grep -q "${expected_code_pattern}" "${script_path}"; then
-            #         code_check_passed=false
-            #         details+=" Required code pattern not found."
-            #     fi
-            # fi
-            # if [[ "$code_check_passed" == true ]]; then
-                 result_status="PASS"
-                 points_awarded=${points_value}
-                 ((total_points_earned += points_awarded))
-            # fi
+            result_status="PASS"
+            points_awarded=${points_value}
+            ((total_points_earned += points_awarded))
         else
             details+=" Output mismatch (Expected: '${expected_output}')."
         fi
@@ -99,26 +87,6 @@ _check_script_output(){
     printf "${CY}%-30s %-10s %-10s ${CR}%s${CW}\n" \
         "${script_basename}.sh" "${result_status}" "${points_awarded}/${points_value}" "${details}"
 }
-
-# _eval(){
-#     target=$1     arg=$2     ret=$3     code=$4     r=false     points=0      value=10
-#     # Evaluating output
-#     if ${bdir}/${target}.sh ${arg} 2>/dev/null | grep -q "${ret}"
-#     then
-#         # Evaluating source code
-#         #if grep -q "${code}" ${bdir}/${target}
-#         #then
-#             r=true
-#             # grading
-#             case ${target} in
-#                             challenge*) (( points = value * 1 )); ((grade+=points)) ;;
-#                 processFile|testString) (( points = value * 3 )); ((grade+=points)) ;;
-#                                      *) (( points = value * 2 )); ((grade+=points)) ;;
-#             esac
-#         #fi
-#     fi
-#     printf "${CG}%-30s${CY}%-20s%-20s${CW}\n" ${bdir}/${target}.sh ${r} ${points}
-# }
 
 # --- Verification Logic ---
 
@@ -184,74 +152,23 @@ do
 done
 _print_line
 
-# printf "${CG}%-30s%-20s%-20s${CW}\n" SCRIPT RESULT POINTS
-# _print_line
-# for t in "${targets[@]}"
-# do
-#     case ${t} in
-#         challenge1)  _eval      "${t}" "" \
-#                                 "Hello, world!" ;;
-
-#         challenge2)  _eval      "${t}" "" \
-#                                 "Linux" ;;
-
-#         challenge3)  _eval      "${t}" "" \
-#                                 "Hello Alice, Welcome to Wonderland!" ;;
-
-#         challenge4)  _eval      "${t}" "" \
-#                                 "File /etc/passwd found" ;;
-
-#         challenge5)  _eval      "${t}" "Learning Linux challenging it also fun very rewarding" \
-#                                 "Learning Linux is challenging but it is also fun and very rewarding" ;;
-
-#         challenge6)  _eval      "${t}" "" \
-#                                 "Multiplication: 50" ;;
-
-#         challenge7)  _eval      "${t}" "" \
-#                                 "We are in the era of AI and agriculture is still more important in the year 2024!" ;;
-
-#         challenge8)  _eval      "${t}" "" \
-#                                 "/etc/passwd exists!" ;;
-
-#         challenge9)  _eval      "${t}" "" \
-#                                 ". Like in the Beatles song, Hello, Goodbye!" ;;
-
-#         homeChecker) _eval      "${t}" "" \
-#                                 "/home/goneuser" ;;
-
-#         processFile) _eval      "${t}" "/etc/passwd" \
-#                                 "Method" ;;
-
-#         rabbitJumps) _eval      "${t}" "" \
-#                                 "The Rabbit made it!" ;;
-
-#         testString)  _eval      "${t}" "1" \
-#                                 "Binary or positive integer" ;;
-
-#         userValidator) _eval    "${t}" "" \
-#                                 "sync password is NOT SET" ;;
-
-#         *) echo "Unknown" ;;
-#     esac
-# done
-# _print_line
-
-
 # --- Grade Calculation & Reporting ---
 
 echo "" # Add a blank line for spacing
 _print_line
-if [[ ${total_questions} -gt 0 ]]; then
-    # Calculate grade using bc for floating point
-    grade=$(echo "scale=2; (100 / ${total_questions}) * ${correct_answers}" | bc -l)
-    # Print rounded grade
-    printf "${CP}Assignment ${assignment} Result: ${CG}%d%%${CW} (%d/%d checks passed)\n" \
-           "$(printf '%.0f' ${grade})" \
-           "${correct_answers}" \
-           "${total_questions}"
+
+local grade_percentage=0
+if [[ ${max_possible_points} -gt 0 ]]; then
+    # Calculate grade percentage using bc for floating point
+    grade_percentage=$(echo "scale=2; (100 / ${max_possible_points}) * ${total_points_earned}" | bc -l)
+    printf "${CP}Assignment ${assignment} Result: ${CG}%d%%${CW} (%d/%d points earned)\n" \
+           "$(printf '%.0f' ${grade_percentage})" \
+           "${total_points_earned}" \
+           "${max_possible_points}"
 else
-    echo -e "${CR}No questions were checked. Grade cannot be calculated.${CW}"
-    grade=0 # Assign 0 if no questions were run
+    echo -e "${CR}No questions were checked or max points is zero. Grade cannot be calculated.${CW}"
+    grade_percentage=0 # Assign 0 if no questions were run
 fi
+
 _print_line
 source <(curl -s --fail -L -H 'Cache-Control: no-cache' "${ENCODER_SCRIPT_URL}") "${grade}" "${assignment}"
